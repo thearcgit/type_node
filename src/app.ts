f199ea59ca1ff7ab9b3eebdb8701cd64508c7a63
Abhay <abhay18d@gmail.com>
2025-10-15 17:44:05 +0530

    GA test 6

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
 import express from "express";
-import type { Request, Response } from "express"; 
+import type { Request, Response } from "express";
 
 import routes from "./routes/index.routes.js"
 
 const app = express()
 
 app.use(express.json())
-app.get(`/`,(req:Request,res:Response)=> {
-    console.log("server is running with CI  test")
-    res.json({message:`Server is running!`})
-} )
+app.get(`/`, (req: Request, res: Response) => {
+    console.log("server is running with CI test")
+    res.json({ message: `Server is running!` })
+})
 
 
-app.use(`/api`,routes)
+app.use(`/api`, routes)
 
 
 export default app
